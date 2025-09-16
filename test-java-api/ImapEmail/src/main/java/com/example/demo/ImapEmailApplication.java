40d519f6373a4f1fa3c8f80a5d8f37eab65431bf
ruchitha6000 <ruchitha6000@gmail.com>
2025-09-16 13:00:27 +0530

    Add files via upload

---
+++ test-java-api/ImapEmail/src/main/java/com/example/demo/ImapEmailApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@ComponentScan(basePackages = {"com.example.demo", "com.example.scheduler", "com.example.service"})
+public class ImapEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImapEmailApplication.class, args);
+	}
+
+}
